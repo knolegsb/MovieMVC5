9742424746fa860762825f774ecbce7b14da238c
knolegsb <seon2you@hotmail.com>
2015-10-22 12:55:53 -0700

    added view

--- MovieMVC5/Controllers/HelloWorldController.cs
+++ MovieMVC5/Controllers/HelloWorldController.cs
@@ -9,15 +9,29 @@ namespace MovieMVC5.Controllers
     public class HelloWorldController : Controller
     {
         // GET: HelloWord
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            //return "This is my <b>default</b> action...";
+            return View();
         }
 
         // GET: /HelloWorld/Welcome/
-        public string Welcome()
+        //public string Welcome(string name, int numTimes = 1) 
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        //}
+
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return "This is the welcome action method...";
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
+
+        ////public string Welcome(string name, int ID = 1)
+        ////{
+        ////    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        ////}
     }
 }
