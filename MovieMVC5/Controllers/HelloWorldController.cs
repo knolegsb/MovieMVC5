1cf2ac652c230a7f04de3375124db2299f9c3627
knolegsb <seon2you@hotmail.com>
2015-10-22 12:09:37 -0700

    add a controller

---
+++ MovieMVC5/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieMVC5.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWord
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the welcome action method...";
+        }
+    }
+}
